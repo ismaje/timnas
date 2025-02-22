527ba763694dce2b822d10360b4e83395a9eb0b7
ismaje <108852629+ismaje@users.noreply.github.com>
2025-02-22 22:29:18 +0700

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from sklearn.svm import SVC
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
+
+# Memuat model SVM dan TF-IDF vectorizer yang telah disimpan
+with open('svm_model (1).pkl', 'rb') as f:
+    svm_classifier = pickle.load(f)
+
+print("Kelas model:", svm_classifier.classes_)
+
+with open('tfidf_vectorizer (1).pkl', 'rb') as f:
+    tfidf_vectorizer = pickle.load(f)
+
+# Fungsi untuk melakukan prediksi terhadap input teks
+def predict_sentiment(input_text):
+    input_tfidf = tfidf_vectorizer.transform([input_text])  # Mengubah teks input menjadi fitur TF-IDF
+    prediction = svm_classifier.predict(input_tfidf)  # Prediksi menggunakan model SVM
+    return 'Positif' if prediction[0] == 1 else 'Negatif'  # Mengembalikan hasil prediksi
+
+# Header dan penjelasan aplikasi
+st.title("Analisis Sentimen ⚽︎")
+st.write("Masukkan kalimat di bawah ini, dan sistem akan mendeteksi apakah kalimat tersebut positif atau negatif.")
+
+
+# Input dari pengguna
+user_input = st.text_input("📝 Masukkan kalimat untuk diprediksi:", "")
+
+# Tombol untuk memulai prediksi
+if st.button('🔍 Prediksi Sentimen'):
+    if user_input:  # Pastikan ada input dari pengguna
+        sentiment = predict_sentiment(user_input)  # Prediksi sentimen
+        
+        # Menentukan warna dan emoji berdasarkan sentimen
+        if sentiment == "Positif":
+            st.success(f"😊 Sentimen dari kalimat: *{sentiment}*")
+        else:
+            st.error(f"😠 Sentimen dari kalimat: *{sentiment}*")
+    else:
+        st.warning("⚠ Silakan masukkan kalimat terlebih dahulu.")
